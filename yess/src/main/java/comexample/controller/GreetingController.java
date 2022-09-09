382e3e70d77e803b2e8b374361a2a754714b40b4
nguyenphuc355 <nguyenvanphuca9999@gmail.com>
2022-09-09 19:37:58 +0700

    up

---
+++ yess/src/main/java/comexample/controller/GreetingController.java
@@ -0,0 +1,12 @@
+package comexample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Test {
+    @GetMapping("/")
+    public String test(){
+       return "/test";
+    }
+}
