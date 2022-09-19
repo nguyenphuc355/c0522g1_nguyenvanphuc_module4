6b265879f25a3e8f52a643a4632129545af78b3a
nguyenphuc355 <nguyenvanphuca9999@gmail.com>
2022-09-19 19:31:23 +0700

    up

---
+++ 07_spring_data_repository/exxercise/blog_app/src/main/java/com/example/blog_app/repository/IBlogRepository.java
@@ -0,0 +1,21 @@
+package com.example.blog_app.repository;
+
+import com.example.blog_app.model.BlogApp;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBlogRepository extends JpaRepository<BlogApp, Integer> {
+    BlogApp findById(int id);
+
+    @Query(value = "select * from blog_app where name_blog like %:name%", nativeQuery = true)
+    Page<BlogApp> searchName(@Param("name") String name,Pageable pageable);
+
+    @Query(value = "select * from blog_app order by date_created ",nativeQuery = true)
+    Page<BlogApp> findAll(Pageable pageable);
+
+}
---
+++ 07_spring_data_repository/exxercise/blog_app/src/main/java/com/example/blog_app/service/IBlogService.java
@@ -0,0 +1,23 @@
+package com.example.blog_app.service;
+
+import com.example.blog_app.model.BlogApp;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface IBlogService {
+    List<BlogApp> findAll();
+
+    void save(BlogApp blogApp);
+
+    BlogApp findById(int id);
+
+    void update(BlogApp blogApp);
+
+    void remove(int id);
+
+    Page<BlogApp> findAll(Pageable pageable);
+
+    Page<BlogApp> findByName(Pageable pageable, String name);
+}
